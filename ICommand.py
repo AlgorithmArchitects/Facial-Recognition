ad950907c04e73322ebaf7198b712db6ae9bfbce
AndrewDorn <dornadigital@gmail.com>
2016-09-06 12:26:42 -0500

    Update ICommand.py
    Add comments to explain usage.

--- ICommand.py
+++ ICommand.py
@@ -4,15 +4,15 @@ class ICommand(object):
 	__metaclass__ = abc.ABCMeta
 	@staticmethod
 	@abc.abstractmethod
-	def name():
+	def name():#returns the name that will be used to recognize the console command
 		raise NotImplementedError
 	
 	@staticmethod
 	@abc.abstractmethod
-	def description():
+	def description():#returns a string describing function usage
 		raise NotImplementedError
 	
 	@staticmethod
 	@abc.abstractmethod
-	def performCommand(argumentList, openCrWrapper):
+	def performCommand(argumentList, openCrWrapper):#accepts an array of arguments and an OpenCvWrapper and performs the given operation
 		raise NotImplementedError
