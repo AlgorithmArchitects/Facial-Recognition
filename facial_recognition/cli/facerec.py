ef09b74ba5781c7fe95a030757b5e70120fd6aea
Carter Thayer <carter@debian>
2016-09-11 09:50:23 -0500

    Add train command to cli

--- facial_recognition/cli/facerec.py
+++ facial_recognition/cli/facerec.py
@@ -1,6 +1,6 @@
 import click
 from facial_recognition.cli.csv.create_csv import csv
-from facial_recognition.cli.recognize.face_recognizer import recognize
+from facial_recognition.cli.recognize.face_recognizer import recognize, train
 
 
 @click.group()
@@ -9,6 +9,7 @@ def facerec():
 
 facerec.add_command(csv)
 facerec.add_command(recognize)
+facerec.add_command(train)
 
 
 if __name__ == "__main__":
