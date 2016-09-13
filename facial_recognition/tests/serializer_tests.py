ea404b426c614b81ba12738a22c3e29ed897896d
AndrewDorn <dornadigital@gmail.com>
2016-09-13 10:29:55 -0500

    My client isn't committing changes properly. Updating manually.

--- facial_recognition/tests/serializer_tests.py
+++ facial_recognition/tests/serializer_tests.py
@@ -1,6 +1,11 @@
 import unittest
-from facial_recognizer.core.RecognizerSerializer import RecognizerSerializer
+from core.RecognizerSerializer import RecognizerSerializer
 
 class TestSerializerMethods(unittest.TestCase):
-    RecognizerSerializer.serialize_recognizer(2)
-    
+    def test_basic_serialize_deserialize(self):
+        RecognizerSerializer.serialize_recognizer(2)
+        self.assertEqual(2, RecognizerSerializer.deserialize_recognizer())
+
+    def test_can_overwrite_serialized_data(self):
+        RecognizerSerializer.serialize_recognizer(2)
+        RecognizerSerializer.serialize_recognizer(2)
