696c5f8794307555a0564ce02cb662f074898547
AndrewDorn <dornadigital@gmail.com>
2016-09-13 11:39:13 -0500

    Updated serializer to use constant user directory.

--- facial_recognition/core/RecognizerSerializer.py
+++ facial_recognition/core/RecognizerSerializer.py
@@ -1,12 +1,14 @@
 import pickle
+import constants
+import os
 class RecognizerSerializer():
     @staticmethod
     def deserialize_recognizer():
-        if(os.path.isfile("C:/csv_recognizer_info")):
-            return pickle.load(open("C:/csv_recognizer_info", "rb"))
+        if(os.path.isfile(constants.USER_DATA_DIRECTORY + "\\csv_recognizer_info")):
+            return pickle.load(open(constants.USER_DATA_DIRECTORY + "\\csv_recognizer_info", "rb"))
         else:
             raise FileNotFoundError
     
     @staticmethod
     def serialize_recognizer(recognizer):
-        pickle.dump(recognizer, open("C:/csv_recognizer_info", "wb"))
+        pickle.dump(recognizer, open(constants.USER_DATA_DIRECTORY + "\\csv_recognizer_info", "wb"))
