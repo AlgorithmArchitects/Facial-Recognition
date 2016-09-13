6d1ed84aca6cfd03ad91517914492b1abdcca72f
AndrewDorn <dornadigital@gmail.com>
2016-09-13 13:00:34 -0500

    Upgraded pickle to dill.

--- facial_recognition/core/RecognizerSerializer.py
+++ facial_recognition/core/RecognizerSerializer.py
@@ -1,12 +1,15 @@
 import pickle
+import constants
+import os
+import dill
 class RecognizerSerializer():
     @staticmethod
     def deserialize_recognizer():
-        if(os.path.isfile("C:/csv_recognizer_info")):
-            return pickle.load(open("C:/csv_recognizer_info", "rb"))
+        if(os.path.isfile(constants.USER_DATA_DIRECTORY + "\\csv_recognizer_info")):
+            return dill.load(open(constants.USER_DATA_DIRECTORY + "\\csv_recognizer_info", "rb"))
         else:
             raise FileNotFoundError
     
     @staticmethod
     def serialize_recognizer(recognizer):
-        pickle.dump(recognizer, open("C:/csv_recognizer_info", "wb"))
+        dill.dump(recognizer, open(constants.USER_DATA_DIRECTORY + "\\csv_recognizer_info", "wb"))
